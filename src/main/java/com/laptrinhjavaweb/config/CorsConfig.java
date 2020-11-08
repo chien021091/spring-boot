2c32c0ec0bd4ac012bb1e897df2d7174dbe11511
TRAN QUANG CHIEN <qctran1991@gmail.com>
2020-11-08 01:36:58 +0100

    spring cors

---
+++ src/main/java/com/laptrinhjavaweb/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.laptrinhjavaweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public WebMvcConfigurerAdapter corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("*")
+				.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+}
